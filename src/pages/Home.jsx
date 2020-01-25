34797adbdfb6244d833e611bb1b7ce1cc485a4a6
publizm <publizm@gmail.com>
2020-01-25 16:46:30 +0900

    feat: added to Get Books data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import withAuth from "../hocs/withAuth";
+import axios from "axios";
 
 const Home = ({ token }) => {
+  const [books, setBooks] = useState();
+
+  const getBooks = async () => {
+    try {
+      const response = await axios.get("https://api.marktube.tv/v1/book", {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getBooks();
+  }, [getBooks]);
+
   return (
     <>
       <ul>
