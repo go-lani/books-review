d4216b17a392d4ab44f6528a5f8fb0c84ff132a0
publizm <publizm@gmail.com>
2020-02-02 18:14:33 +0900

    feat: created signinresult feedback action and reducer

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
-import reducer from "./reducer";
+import reducers from "./reducers";
 
 // const token = localStorage.getItem('token');
 // const store = createStore(reducer, token);
@@ -7,8 +7,9 @@ import reducer from "./reducer";
 // 모듈을 불러올때 초기값 세팅을 원하는 타이밍에 세팅하도록 지연시키기 위해 function으로 export
 export default function create(initailState) {
   return createStore(
-    reducer,
+    reducers,
     initailState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__(),
   );
 }
